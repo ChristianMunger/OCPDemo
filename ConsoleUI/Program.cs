baa213cfe9f09d62db524bfd8384eb4c32d5ec2a
Christian Munger <christianmngr2@gmail.com>
2023-07-12 21:03:28 -0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using OCPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // open for extension, closed for modification
+            // OCP is okay in dev, in prod stuff should not really be changed except for bugs
+            // use extract interfaces 
+            List<IApplicantModel> applicants = new List<IApplicantModel>
+            {
+                new PersonModel { FirstName = "Tim", LastName = "Corey" },
+                new ManagerModel { FirstName = "Sue", LastName = "Storm" },
+                new ExecutiveModel { FirstName = "Nancy", LastName = "Roman"}
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountProcessor = new Accounts();
+
+            foreach (var person in applicants)
+            {
+                employees.Add(person.AccountProcessor.Create(person));
+            }
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{ emp.FirstName } { emp.LastName }: { emp.EmailAddress } IsManager: { emp.IsManager } IsExecutive: { emp.IsExecutive }");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OCPLibrary/Accounts.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCPLibrary
+{
+    public class Accounts : IAccounts
+    {
+        public EmployeeModel Create(IApplicantModel person)
+        {
+            EmployeeModel output = new EmployeeModel();
+
+            output.FirstName = person.FirstName;
+            output.LastName = person.LastName;
+            output.EmailAddress = $"{ person.FirstName.Substring(0, 1) }{person.LastName}@acme.com";
+
+            return output;
+
+
+            // violating ocp, modifying method
+            //if (person.TypeOfEmployee == EmployeeType.Manager)
+            //{
+            //    output.IsManager = true;
+            //}
+            //if (person.TypeOfEmployee == EmployeeType.Executive)
+            //{
+            //    output.IsExecutive = true;
+            //}
+
+            //switch (person.TypeOfEmployee)
+            //{
+            //    case EmployeeType.Manager:
+            //        output.IsManager = true;
+            //        break;
+            //    case EmployeeType.Staff:
+
+            //        break;
+            //    case EmployeeType.Executive:
+            //        output.IsExecutive = true;
+            //        output.IsManager = true;
+            //        break;
+            //}
+            //return output;
+        }
+
+
+    }
+}
